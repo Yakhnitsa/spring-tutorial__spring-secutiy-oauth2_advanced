0e109693e21def6120645ddb93fad80f188381b9
Yuriy Yakhnitsa <transpmailbox@gmail.com>
2019-12-04 16:26:15 +0200

    Complete facebook authentification and add vue js

--- src/main/java/com/yurets_y/springsecurityoauth2/SpringSecurityOauth2Application.java
+++ src/main/java/com/yurets_y/springsecurityoauth2/SpringSecurityOauth2Application.java
@@ -2,13 +2,6 @@ package com.yurets_y.springsecurityoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @SpringBootApplication
 public class SpringSecurityOauth2Application{
